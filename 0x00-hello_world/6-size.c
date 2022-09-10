c77e13c21c33b1ef244275df677c57f4e43ab583
Mohammed Abdu <mamedaw2@gmail.com>
2022-09-10 12:26:07 -0700

    sizeof

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,14 +3,12 @@
 
 
 /**
+ * main - Entry point
  *
- *  * main - Entry point
  *
- *   *
+ * Return: Always 0 (Success)
  *
- *    * Return: Always 0 (Success)
- *
- *     */
+ */
 
 int main(void)
 
