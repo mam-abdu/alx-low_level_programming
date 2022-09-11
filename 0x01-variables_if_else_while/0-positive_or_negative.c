ecae4a523cf7b9c17244e007276ffa32487288ba
Mohammed Abdu <mamedaw2@gmail.com>
2022-09-11 06:48:06 -0700

    if

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -30,17 +30,14 @@ int main(void)
 
 
 
-				if (n > 0)						
+			if (n > 0)						
 				printf("%d is positive\n", n);
 
-				else if (n == 0)
-						
+				else if (n == 0)			
 				printf("%d is zero\n", n);
 
 				else
-						
 				printf("%d is negative\n", n);
-
 
 
 				return (0);
