e00c993ddabf48d1e3d5a784187436c9a2d972a1
Mohammed Abdu <mamedaw2@gmail.com>
2022-09-11 13:14:01 -0700

    combo3

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -14,40 +14,27 @@
  * Return: Always 0 (Success)
  *
  */
-
-
-
 int main(void)
 
 {
-
 		int d, p;
-
-
 
 		for (d = '0'; d < '9'; d++)
 
 		{
-
 			for (p = d + 1; p <= '9'; p++)
 
 			{
-
 				if (p != d)
 
 				{
-
 					putchar(d);
 
 					putchar(p);
 
-
-
 					if (d == '8' && p == '9')
 
 						continue;
-
-
 
 					putchar(',');
 
@@ -61,8 +48,5 @@ int main(void)
 
 		putchar('\n');
 
-
-
 		return (0);
-
 }
