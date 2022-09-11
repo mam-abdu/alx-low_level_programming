0c9a81dec8af30dd45451726a22bb294882cb954
Mohammed Abdu <mamedaw2@gmail.com>
2022-09-11 09:30:47 -0700

    alphabets

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -24,12 +24,10 @@ int main(void)
 		char low;
 
 
+		for (low = 'a'; low <= 'z'; low++)
+						putchar(low);
 
-			for (low = 'a'; low <= 'z'; low++)
-							putchar(low);
-
-				for (low = 'A'; low <= 'Z'; low++)
-
+			for (low = 'A'; low <= 'Z'; low++)
 							putchar(low);
 
 					putchar('\n');
