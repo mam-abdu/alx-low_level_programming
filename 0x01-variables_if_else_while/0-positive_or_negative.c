44cb9e0967a72fbac133423b6fef8c1442735bd6
Mohammed Abdu <mamedaw2@gmail.com>
2022-09-11 06:22:12 -0700

    if, else

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,15 +4,18 @@
 
 #include <time.h>
 
+
+
+
 /**
  *
- *  * main - Entry point
+ * main - Entry point
  *
- *   *
  *
- *    * Return: Always 0 (Success)
  *
- *     */
+ * Return: Always 0 (Success)
+ *
+ */
 
 int main(void)
 
