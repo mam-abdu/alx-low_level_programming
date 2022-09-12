dfc26718fe63a9aed9fab2188f557ad4fa030e3a
Mohammed Abdu <mamedaw2@gmail.com>
2022-09-11 23:11:47 -0700

    puts

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -13,9 +13,9 @@
 
 int main(void)
 {
-		write(2, "and that piece of art is useful"
+		write(2, "and that piece of art is useful
 
-				" - Dora Korpar, 2015-10-19\n", 61);
+				- Dora Korpar, 2015-10-19\n", 61);
 
 			return (1);
 
