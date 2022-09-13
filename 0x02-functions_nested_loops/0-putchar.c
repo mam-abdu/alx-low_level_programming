2f90f1583354ddc54e57d1ff5de060d3bd037c48
Mohammed Abdu <mamedaw2@gmail.com>
2022-09-13 12:22:40 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -15,10 +15,10 @@
 int main(void)
 {
 		char word[8] = "_putchar";
-			int i;
+		int i;
 
-				for (i = 0; i < 8; i++)
-						_putchar(word[i]);
+		for (i = 0; i < 8; i++)
+				_putchar(word[i]);
 
 				_putchar('\n');
 
