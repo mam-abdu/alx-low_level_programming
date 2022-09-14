93d9bbec8563d0e85a2f5019a7c2c5e835a0370b
Mohammed Abdu <mamedaw2@gmail.com>
2022-09-14 13:32:45 -0700

    fib

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -15,15 +15,13 @@ int main(void)
 		{
 			fibsum = fib1 + fib2;
 			if (fibsum > 4000000)
-			{
-			break;
-			}
+				break;
+			
 			if ((fibsum % 2) == 0)
-			{
 				tot_sum += fibsum;
 				fib1 = fib2;
 				fib2 = fibsum;
-			}
+			
 		}
 		printf("%.0f\n", tot_sum);
 
