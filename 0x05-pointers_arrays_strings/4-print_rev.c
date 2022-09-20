5f150112ee1bf31fb8618a60b649b9cf2f6d46c0
Mohammed Abdu <mamedaw2@gmail.com>
2022-09-20 02:51:35 -0700

    reverese

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,19 +2,20 @@
 
 /**
  * print_rev - Prints a string in reverse.
- 
+ *
  * @s: The string to be printed.
  *
  */
 void print_rev(char *s)
 {
-		int len = 0, index;
+	int len = 0, index;
 
-		while (s[index++])
-			len++;
+	while (s[index++])
+		len++;
 
-		for (index = len - 1; index >= 0; index--)
-			_putchar(s[index]);
+	for (index = len - 1; index >= 0; index--)
+		_putchar(s[index]);
 
-		_putchar('\n');
+	_putchar('\n');
 }
+
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,17 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+/**
+ * File: main.h
+ *
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x05-pointers_arrays_strings directory.
+ */
+
+#include <stdlib.h>
+
+
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -21,3 +35,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 
 int _atoi(char *s);
+
+#endif
